ffdbbd93d6da0057da5d4f26648f13d2e55b8b13
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2024-01-01 23:22:11 +0600

    nav bar added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="max-w-[1440px] mx-auto bg-[#F5F4F2]">
+      <Navbar />
+      <div className="grid grid-cols-3 pt-28">
+        <div className="min-h-screen max-h-screen overflow-y-auto">
+          {/* Add your content here */}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta officiis maiores possimus, corporis impedit necessitatibus quibusdam deserunt modi quas labore iusto aspernatur. Corporis itaque delectus illum laborum blanditiis autem dolorem?
+        </div>
+        <div className="min-h-screen max-h-screen overflow-y-auto h-screen">
+          {/* Add your content here */}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores repellendus laudantium inventore nulla! Debitis praesentium quas asperiores mollitia quae deleniti odit aspernatur. Quidem velit assumenda rem ab quod iste blanditiis voluptatum necessitatibus repellendus sunt aperiam perspiciatis, quos dolore quasi facere laudantium deleniti. Molestias error laborum est illo, dolor debitis optio quisquam eaque possimus consectetur. Perspiciatis cupiditate tempora atque, tenetur aspernatur ab maiores neque nihil aliquam rerum labore magnam in mollitia perferendis, delectus dolorum voluptatibus, omnis expedita. Nisi deserunt quia cumque, ipsum consectetur assumenda sed facere, sequi accusantium quidem ducimus tempore culpa reprehenderit, facilis similique deleniti molestias inventore odit numquam officiis eaque laudantium. Neque ipsum blanditiis iure consequuntur nesciunt cupiditate magnam, repellat explicabo ut dicta aliquam quam molestiae laudantium quia voluptatibus vitae quas distinctio rerum eum itaque cum ullam ea voluptates delectus? Laborum, voluptates amet. Saepe, labore eligendi, sit unde adipisci vel iste minima culpa modi magnam esse molestiae eveniet nam accusamus quaerat ullam. Repudiandae nostrum maiores impedit molestiae cum officia dolor totam at, iure modi ea tempore architecto minus aperiam similique perspiciatis distinctio dolores, voluptatibus omnis. Distinctio repellendus enim id officia molestias sed quam, eligendi obcaecati minima quibusdam? Dolor praesentium nihil accusamus sequi excepturi ad nemo, repellendus ea. Illo, iure!Fugit ducimus nobis a quaerat odio repellendus ipsam! Commodi non autem aliquid assumenda? Suscipit veritatis eos nostrum illum ullam corporis consequuntur rerum!
+        </div>
+        <div className="min-h-screen max-h-screen overflow-y-auto">
+          {/* Add your content here */}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam in accusantium iusto voluptas alias, minima natus id aspernatur dicta rerum nemo excepturi nobis quaerat pariatur ducimus nam omnis ratione inventore.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { GrLocationPin } from "react-icons/gr";
+import { IoIosArrowDown } from "react-icons/io";
+import { TbWorld } from "react-icons/tb";
+const Navbar = () => {
+  return (
+    <div className=" fixed w-full top-0 bg-[#F5F4F2]">
+      <div className="navbar px-4 lg:px-6 py-3">
+        {/* nav start */}
+        <div className="flex-1 flex items-center gap-4">
+          <div>
+            <img
+              className=" h-8 w-8 lg:h-14 lg:w-14"
+              src="https://i.ibb.co/r6y3mmC/spiral.png"
+              alt=""
+            />
+          </div>
+          {/* location */}
+          <div className=" flex items-center gap-2 text-base w-fit lg:text-xl font-extrabold border-2 border-gray-300 rounded-2xl px-3 lg:px-5 py-1.5 lg:py-3">
+            <GrLocationPin />{" "}
+            <span className=" text-xs lg:text-base font-medium">Mirpur 10, Dhaka</span>{" "}
+            <IoIosArrowDown />
+          </div>
+        </div>
+        {/* nav end */}
+        <div className="flex-none flex gap-2 lg:gap-5 items-center">
+          <div className=" flex flex-col items-center">
+            <TbWorld className=" text-lg lg:text-2xl" />
+            <span className=" text-sm lg:text-2xl">English</span>
+          </div>
+          <div>
+            <button className=" bg-[#E7E6E4] rounded-xl lg:rounded-2xl px-4 lg:px-8 py-1 lg:py-4 flex justify-center items-center">Login</button>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-gray-300 h-[1px]"></div>
+    </div>
+  );
+};
+
+export default Navbar;
